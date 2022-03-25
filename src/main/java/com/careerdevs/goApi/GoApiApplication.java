20e891f6ce06fb3c9905fe4e57ea71555dfa44fe
jailsonveiga <jmdaveiga2012@gmail.com>
2022-03-25 13:11:49 -0400

    goApi GoApiAplication.java

---
+++ src/main/java/com/careerdevs/goApi/GoApiApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.goApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoApiApplication.class, args);
+	}
+
+}
