492ee02643abac617da9b782985bc9bb63f4894c
jailsonveiga <jmdaveiga2012@gmail.com>
2022-03-25 13:12:52 -0400

    goApi GoApplicationTests.java

---
+++ src/test/java/com/careerdevs/goApi/GoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.goApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
